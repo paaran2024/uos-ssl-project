3fce649bc71c6f670348b4ef8dd674b04c2ddcd5
possiroom <178400765+possiroom@users.noreply.github.com>
2025-11-27 15:28:52 +0900

    file 수정
    merge안해도 됨!!

--- app/lib/picture_tab.dart
+++ app/lib/picture_tab.dart
@@ -1,5 +1,5 @@
 import 'dart:typed_data';
-import 'package:app/media_store_saver.dart';
+import 'package:contact/media_store_saver.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
--- app/lib/video_tab.dart
+++ app/lib/video_tab.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'dart:async';
 import 'dart:typed_data';
-import 'package:app/media_store_saver.dart';
+import 'package:contact/media_store_saver.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:video_player/video_player.dart';
